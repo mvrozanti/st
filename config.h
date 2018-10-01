dc391bdd81d7320da9e78e76625061428f79d913
Marcelo <mvrozanti@hotmail.com>
2018-10-01 13:51:33 -0300

    changed st

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "Droid Sans Mono Slashed for Powerline:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Droid Sans Mono Slashed for Powerline:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
